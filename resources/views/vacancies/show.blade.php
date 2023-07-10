ab6445f68313b140b1ce13102c6284a6b9f79eec
Alexey Dobrynin <ag.dobrynin@gmail.com>
2023-07-10 22:02:20 +0400

    feat: Show details of vacancy at vacancy page.

--- resources/views/vacancies/show.blade.php
+++ resources/views/vacancies/show.blade.php
@@ -2,5 +2,9 @@
     <x-ui.breadcrumbs
         class="mb-4"
         :links="['Home' => '/', 'Vacancies' => route('vacancies.index'), $vacancy['title'] => null]"/>
-    <x-vacancy.card :$vacancy />
+    <x-vacancy.card :$vacancy>
+        <x-slot:description>
+            {!! nl2br(e($vacancy->description)) !!}
+        </x-slot:description>
+    </x-vacancy.card>
 </x-layouts.app>
