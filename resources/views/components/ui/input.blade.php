247039491380756fb31c96898c3264f5a6680a47
Alexey Dobrynin <ag.dobrynin@gmail.com>
2023-07-12 13:12:54 +0400

    refactor: AlpineJs action for input component - show and hide clear button.

--- resources/views/components/ui/input.blade.php
+++ resources/views/components/ui/input.blade.php
@@ -9,7 +9,9 @@
 <label>
     @if($label)<div class="font-semibold mb-1 ms-1">{{ $label }}</div>@endif
     <div class="relative"
+         x-init="val = '{{$value}}'"
          x-data="{
+            val: '',
             clear() {
                 this.$refs.input.value = '';
                 this.$refs.input.form?.submit();
@@ -17,10 +19,10 @@
          }">
         @if($clearBtn)
             <button
-                x-show="$refs.input.value.length"
+                x-show="val.length"
                 type="button"
                 class="absolute top-0 right-0 flex h-full items-center pr-2"
-                x-on:click="clear()">
+                x-on:click="clear">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
                      stroke="currentColor" class="h-4 w-4 text-slate-500">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
@@ -29,6 +31,7 @@
         @endif
         <input type="{{ $type }}"
                x-ref="input"
+               x-model="val"
                {{ $attributes->class([
                    'form-input rounded-md border-slate-200 w-full',
                    '!border-red-300 text-red-600' => $errors->has($name),
