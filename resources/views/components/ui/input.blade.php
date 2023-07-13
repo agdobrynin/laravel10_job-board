63118360020a37bc1d27fc17a0e6d1688b671079
Alexey Dobrynin <ag.dobrynin@gmail.com>
2023-07-13 09:43:02 +0400

    refactor: Input component clear error on input

--- resources/views/components/ui/input.blade.php
+++ resources/views/components/ui/input.blade.php
@@ -6,23 +6,27 @@
     'type' => 'text',
     'clearBtn' => true,
 ])
-<label>
+<label
+    x-init="val = '{{$value}}'; hasError = Boolean({{ $errors->has($name) }})"
+    x-data="{
+        val: '',
+        hasError: false,
+        clear() {
+            this.$refs.input.value = '';
+        },
+        submit() {
+            this.$refs.input.form?.submit();
+        },
+    }"
+>
     @if($label)<div class="font-semibold mb-1 ms-1">{{ $label }}</div>@endif
-    <div class="relative"
-         x-init="val = '{{$value}}'"
-         x-data="{
-            val: '',
-            clear() {
-                this.$refs.input.value = '';
-                this.$refs.input.form?.submit();
-            }
-         }">
+    <div class="relative">
         @if($clearBtn)
             <button
                 x-show="val.length"
                 type="button"
                 class="absolute top-0 right-0 flex h-full items-center pr-2"
-                x-on:click="clear">
+                x-on:click="clear(); submit();">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
                      stroke="currentColor" class="h-4 w-4 text-slate-500">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
@@ -32,6 +36,8 @@
         <input type="{{ $type }}"
                x-ref="input"
                x-model="val"
+               x-on:input="hasError = false"
+               :class="{ '!border-red-300 text-red-600': hasError }"
                {{ $attributes->class([
                    'form-input rounded-md border-slate-200 w-full',
                    '!border-red-300 text-red-600' => $errors->has($name),
@@ -42,7 +48,7 @@
                placeholder="{{ $placeholder }}" />
     </div>
     @error($name)
-        <div class="mt-1 text-xs text-red-500">
+        <div x-show="hasError" class="mt-1 text-xs text-red-500">
             {{ $message }}
         </div>
     @enderror
